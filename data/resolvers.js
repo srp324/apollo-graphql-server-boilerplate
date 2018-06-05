29aa22d406ed96cb335dee1faeb3544bdf2142b3
sagarpatel <srp324@drexel.edu>
2018-06-05 18:35:33 -0400

    Switched resolvers to use the connectors mock data instead of hardcoded values

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,23 +1,25 @@
+import { Author, View } from './connectors';
+
 const resolvers = {
     Query: {
-        author(root, args) {
-            return { id: 1, firstName: 'Hello', lastName: 'World' };
+        author(_, args) {
+            return Author.find({ where: args });
         },
-        allAuthors() {
-            return [{ id: 1, firstName: 'Hello', lastName: 'World' }];
+        allAuthors(_, args) {
+            return Author.findAll();
         }
     },
     Author: {
         posts(author) {
-            return [
-                { id: 1, title: 'A post', text: 'Some text', views: 2 },
-                { id: 2, title: 'Another post', text: 'Some other text', views: 200 }
-            ];
+            return author.getPosts();
         }
     },
     Post: {
         author(post) {
-            return { id: 1, firstName: 'Hello', lastName: 'World' };
+            return post.getAuthor();
+        },
+        views(post) {
+            return View.findOne({ postId: post.id }).then(view => view.views);
         }
     }
 };

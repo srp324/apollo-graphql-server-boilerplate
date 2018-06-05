24c95d617f3a5bc276388f5cf7fdd0cbc7ed0f92
sagarpatel <srp324@drexel.edu>
2018-06-05 17:58:28 -0400

    Added Author and Post typedefs

--- data/schema.js
+++ data/schema.js
@@ -2,8 +2,25 @@ import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import mocks from './mocks';
 
 const typeDefs = `
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  allAuthors: [Author]
+  getFortuneCookie: String # we'll use this later
 }
 `;
 

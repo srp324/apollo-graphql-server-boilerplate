9260a128f7b6a96e94cc9af26176e2695b84f50c
sagarpatel <srp324@drexel.edu>
2018-06-05 21:51:24 -0400

    Implemented MongoDB View Schema

--- data/connectors.js
+++ data/connectors.js
@@ -1,7 +1,24 @@
 import Sequelize from 'sequelize';
 import casual from 'casual';
+import Mongoose from 'mongoose';
 import _ from 'lodash';
 
+//MongoDB =========================================================
+Mongoose.Promise = global.Promise;
+
+const mongo = Mongoose.connect('mongodb://localhost/views', {
+  useMongoClient: true
+});
+
+const ViewSchema = Mongoose.Schema({
+    postId: Number,
+    views: Number,
+  });
+
+const View = Mongoose.model('views', ViewSchema);
+//=================================================================
+
+//SQLLite =========================================================
 const db = new Sequelize('blog', null, null, {
     dialect: 'sqlite',
     storage: './blog.sqlite',
@@ -16,11 +33,11 @@ const PostModel = db.define('post', {
     title: { type: Sequelize.STRING },
     text: { type: Sequelize.STRING },
 });
-
 AuthorModel.hasMany(PostModel);
 PostModel.belongsTo(AuthorModel);
+//=================================================================
 
-// create mock data with a seed, so we always get the same
+// create mock data with a seed, so we always get the same randoms
 casual.seed(123);
 db.sync({ force: true }).then(() => {
     _.times(10, () => {
@@ -32,13 +49,20 @@ db.sync({ force: true }).then(() => {
                 return author.createPost({
                     title: `A post by ${author.firstName} - ` + casual.title,
                     text: casual.sentences(3),
+                }).then((post) => {
+                    return View.update(
+                        { postId: post.id },
+                        { views: casual.integer(0, 100) },
+                        { upsert: true });
                 });
             });
         });
     });
 });
 
+//SQLLite
 const Author = db.models.author;
 const Post = db.models.post;
 
-export { Author, Post };
+
+export { Author, Post, View };
